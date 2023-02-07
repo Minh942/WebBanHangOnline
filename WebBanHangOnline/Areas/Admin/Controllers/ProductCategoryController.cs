55f91492334e51ad4a2a678b71fe2be0aca5d014
Minh Phan <minh.phan@ntq-solution.com.vn>
2023-02-07 15:45:04 +0700

    update product

---
+++ WebBanHangOnline/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        private ApplicationDbContext dbContext = new ApplicationDbContext();
+        // GET: Admin/ProductCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(ProductCategory model)
+        {
+            return View();
+        }
+    }
+}
